16a7534c333e05f1ede873f9ecb811a369aa602b
Diego Sampaio <diego.estaleiro@gmail.com>
2025-03-11 11:44:51 +0000

    HEY-9: test: finalizando testes

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Http\{RedirectResponse};
 
 class QuestionController extends Controller
@@ -12,7 +13,12 @@ class QuestionController extends Controller
     public function store(): RedirectResponse
     {
         $data = request()->validate([
-            'question' => 'required|string|min:10',
+            'question' => [
+                'required',
+                'string',
+                'min:10',
+                new EndWithQuestionMarkRule(),
+            ],
         ]);
 
         Question::query()->create($data);
---
+++ app/Rules/EndWithQuestionMarkRule.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class EndWithQuestionMarkRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!str_ends_with($value, '?')) {
+            $fail('Are you sure that is a question? It is missing a question mark in the end.');
+        }
+    }
+}
